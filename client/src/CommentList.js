37882ac19568f3ffbf45ad4aa2ecf7cf985317ce
SreemanthG <sreemanth2001@gmail.com>
2021-01-02 16:00:04 +0530

    added sync moderation services

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -5,8 +5,22 @@ const CommentList = ({comments}) => {
   
 
     const renderedPosts = comments.map(comment => {
+        let content;
+
+        if(comment.status === 'approved'){
+            content = comment.content;
+        }
+        if(comment.status === 'rejected'){
+            content = 'This comment has been rejected';
+        }
+
+        if(comment.status === 'pending'){
+            content = 'This comment is awaiting moderation';
+        }
+
+        
         return (
-        <li key={comment.id}>{comment.content}</li>
+        <li key={comment.id}>{content}</li>
         );
     }) 
     return ( 
